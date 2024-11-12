fd3f30b56a9a722ea6dffe2135d3af723bb56af9
J S Amogh Krishna <amoghkrishna.cs23@bmsce.ac.in>
2024-11-12 18:58:18 +0530

    Add files via upload

---
+++ Week-4-Shapes-Java.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+abstract class Shape {
+    int dim1;
+    int dim2;
+
+   
+    public Shape() {
+        this.dim1 = 0;
+        this.dim2 = 0;
+
+    }
+
+    public Shape(int dim1, int dim2) {
+        this.dim1 = dim1;
+        this.dim2 = dim2;
+    }
+
+    public abstract void printArea();
+
+}
+
+class Rectangle extends Shape {
+    public Rectangle(int length, int width) {
+        dim1 = length;
+        dim2 = width;
+    }
+
+    public void printArea() {
+
+        int area = dim1 * dim2;
+        System.out.println("Area of Rectangle: " + area);
+
+    }
+}
+
+class Triangle extends Shape {
+    public Triangle(int base, int height) {        
+        dim1 = base;
+        dim2 = height;
+    }
+
+    public void printArea() {
+
+        double area = 0.5 * dim1 * dim2;
+        System.out.println("Area of Triangle: " + area);
+
+    }
+}
+
+class Circle extends Shape {
+    public Circle(int radius) {
+       
+        dim1 = radius;
+        dim2 = 0;
+    }
+
+    public void printArea() {
+
+        double area = Math.PI * dim1 * dim1;
+        System.out.println("Area of Circle: " + area);
+    }
+}
+
+public class shapes {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+	System.out.println("Aaryan Prakash");
+        System.out.println("Enter length and width for Rectangle:");
+
+        int length = in.nextInt();
+        int width = in.nextInt();
+        Shape rectangle = new Rectangle(length, width);
+        rectangle.printArea();
+
+        System.out.println("Enter base and height for Triangle:");
+
+        int base = in.nextInt();
+        int height = in.nextInt();
+        Shape triangle = new Triangle(base, height);
+        triangle.printArea();
+
+        System.out.println("Enter radius for Circle:");
+
+        int radius = in.nextInt();
+        Shape circle = new Circle(radius);
+        circle.printArea();
+
+        in.close();
+    }
+}
