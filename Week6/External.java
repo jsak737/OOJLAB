05ec29394cc6e26bba1c63ba35fe300de1581fc0
J S Amogh Krishna <amoghkrishna.cs23@bmsce.ac.in>
2024-11-13 14:48:06 +0530

    Add files via upload

---
+++ Week6/External.java
@@ -0,0 +1,11 @@
+package SEE;
+import CIE.Student;
+
+public class External extends Student {
+    public int[] seeMarks;
+
+    public External(String usn, String name, int sem, int[] seeMarks) {
+        super(usn, name, sem);
+        this.seeMarks = seeMarks;
+    }
+}
